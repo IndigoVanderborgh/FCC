883a3e3b95b006a858e36ddae097802a8bec4f88
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-11-03 16:57:03 -0700

    1

--- exercise_8.js
+++ exercise_8.js
@@ -33,13 +33,13 @@
 
 
 // Variable declarations
-var StUdLyCapVaR;
+var studlyCapVar;
 var properCamelCase;
-var TitleCaseOver;
+var titleCaseOver;
 
 // Variable assignments
-STUDLYCAPVAR = 10;
-PRoperCAmelCAse = "A String";
-tITLEcASEoVER = 9000;
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
 
 // ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
