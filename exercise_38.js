013fad0146dde818155cd4c20aaf75f0ccf98d65
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-11-09 18:52:09 -0800

    Create exercise_38.js

---
+++ exercise_38.js
@@ -0,0 +1,19 @@
+// Store Multiple Values in one Variable using JavaScript Arrays
+
+// With JavaScript array variables, we can store several pieces of data in one place.
+
+// You start an array declaration with an opening square bracket, end it with a closing square bracket, and put a comma between each entry, like this:
+
+// const sandwich = ["peanut butter", "jelly", "bread"];
+
+
+// ------------------------------------------ exercise ------------------------------------------------------------------------------------------------------------------------------
+// Modify the new array myArray so that it contains both a string and a number (in that order).
+
+// // Only change code below this line
+// const myArray = [];
+
+// Only change code below this line
+const myArray = ['bread', 5];
+
+// ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
