f6ca5e42b625d2c133910c21d283f885768a6c61
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-11-20 21:47:44 -0800

    Create exercise_72.js

---
+++ exercise_72.js
@@ -0,0 +1,59 @@
+// Chaining If Else Statements
+
+// if/else statements can be chained together for complex logic. Here is pseudocode of multiple chained if / else if statements:
+
+// if (condition1) {
+//   statement1
+// } else if (condition2) {
+//   statement2
+// } else if (condition3) {
+//   statement3
+// . . .
+// } else {
+//   statementN
+// }
+
+
+
+// ------------------------------------------ exercise ------------------------------------------------------------------------------------------------------------------------------
+// Write chained if/else if statements to fulfill the following conditions:
+
+// num < 5 - return Tiny
+// num < 10 - return Small
+// num < 15 - return Medium
+// num < 20 - return Large
+// num >= 20 - return Huge
+
+// function testSize(num) {
+//     // Only change code below this line
+
+
+//     return "Change Me";
+//     // Only change code above this line
+//   }
+
+//   testSize(7);
+
+function testSize(num) {
+    // Only change code below this line
+    if () {
+        statement1
+    } else if () {
+        statement2
+    } else if () {
+        statement3
+    } else if () {
+        statement4
+    } else if () {
+        statement5
+    } else {
+        statement6
+    }
+
+    return "Change Me";
+    // Only change code above this line
+}
+
+testSize(7);
+
+// ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
