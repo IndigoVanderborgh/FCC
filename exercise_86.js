fb6382b813b1a8d1680c69aa44be6d7685e01f67
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-11-23 23:23:38 -0800

    Update exercise_86.js

--- exercise_86.js
+++ exercise_86.js
@@ -25,6 +25,7 @@
 
 
 
+
 // ------------------------------------------ exercise ------------------------------------------------------------------------------------------------------------------------------
 // Add a bark property to myDog and set it to a dog sound, such as "woof". You may use either dot or bracket notation.
 const myDog = {
