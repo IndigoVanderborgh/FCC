93c8bbe1444fc10a56f6913aac4d5d5f4e864d06
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-11-29 18:10:37 -0800

    Update exercise_99.js

--- exercise_99.js
+++ exercise_99.js
@@ -33,9 +33,37 @@ multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
 // function multiplyAll(arr) {
 //     let product = 1;
 //     // Only change code below this line
-//       l
+//       
 //     // Only change code above this line
 //     return product;
 //   }
 //   multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
 // ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
+// This one is a bit more complex as we are dealing with a multi dimensional array, or an array with sub arrays. Let's break down the solution in an easy way to understand.
+// The exercise is asking us to change the function so it returns the product of all numbers in the sub array of arr, and we are given product = 1 in the function and a return product, 
+// alongside an array on line 31, which is in the function, and correlates as arr in line 19, meaning: 
+// arr = [
+//     [1, 2], 
+//     [3, 4], 
+//     [5, 6, 7]
+//     ];
+
+// So like our last exercise, lets set up a for loop on line 22, and explain it out. 
+// for (let i = 0; i < arr.length; i++) {
+// Basically an initialization statement of i = 0, a conditional statement that bases it on arr.length, which is 3 as the first level array has 3 elements. Final statement is a increment i by 1.
+// If the condition is true, we move to the next for loop.
+
+// Within that for loop, we have the for loop that access the sub arrays within the array.
+// for (let j = 0; j < arr[i].length; j++) {
+// First things first. Since i has been used for the previous array, we cannot use that anymore as it ties to the previous array. So instead we are going to use j to access our subarray.
+// On top of that, we are going to use arr[i].length, which grabs the length of the sub/inner array
+
+// Within this for loop, we are going to do our objective: to multiply all numbers within the array together. Lets look at line 24. In that, we have the following:
+// product *= arr[i][j]; which is shorthand form of:
+// product = product * arr[i][j];
+// Without product our output is goign to output [1,2,3,4,5,6,7]. By adding our actual multiplicitive line, we multiply each iteration per length. 
+
+// So if we run this for one iteration as an example you can understand the process. The first time will yeild i=0 stated on line 22, which is for the outer loop. a[i] therefore equals a[0] which is:
+// [1,2]
+// and since on line 23, our j = 0, we take the first element of a[i], or thei nner sub array, which is 1. The code then loops through the first inner set, then the secodn and then the third, executing 
+// the 24th line constantly until it returns a false in its condition statement.
