06d16c9a4c3ee8bf7b4bdea0c70fd28a9723a2de
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-11-30 21:19:01 -0800

    Update exercise_101.js

--- exercise_101.js
+++ exercise_101.js
@@ -23,3 +23,17 @@
 // The recursive version of multiply breaks down like this. In the base case, where n <= 0, it returns 1. For larger values of n, it calls itself, but with n - 1. That function call is evaluated in the same way, calling multiply again until n <= 0. At this point, all the functions can return and the original multiply returns the answer.
 
 // Note: Recursive functions must have a base case when they return without calling the function again (in this example, when n <= 0), otherwise they can never finish executing.
+// ------------------------------------------ exercise ------------------------------------------------------------------------------------------------------------------------------
+// Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+function sum(arr, n) {
+    // Only change code below this line
+
+    // Only change code above this line
+  }
+
+// function sum(arr, n) {
+//     // Only change code below this line
+
+//     // Only change code above this line
+//   }
+// ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
