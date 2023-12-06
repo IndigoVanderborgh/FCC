43bcba0a05c9c0dd9fc5461bc06f7444005bb94c
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-12-05 19:06:43 -0800

    Update exercise_104.js

--- exercise_104.js
+++ exercise_104.js
@@ -15,5 +15,13 @@
 // You are calling Math.random(), multiplying the result by 20, then passing the value to Math.floor() to round the value down to the nearest whole number.
 
 // ------------------------------------------ exercise ------------------------------------------------------------------------------------------------------------------------------
+// Use this technique to generate and return a random whole number in the range from 0 to 9.
+function randomWholeNum() {
+    return Math.floor(Math.random() * 10);
+}
 
+
+// function randomWholeNum() {
+//     return Math.random();
+// }
 // ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
